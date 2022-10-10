52319df1331d004af0268a23fe6a63c7762b9f54
Гордиенко Артем Андреевич <gor.aa@skbkontur.ru>
2022-10-10 10:34:11 +0500

    task: 5 and 6

--- src/common/movie-generator.ts
+++ src/common/movie-generator.ts
@@ -2,25 +2,25 @@ import {MockFilms} from '../types/mock-films.type.js';
 import {Genre} from '../types/genre.enum.js';
 
 export default class MovieGenerator {
-    constructor(private readonly mockData: MockFilms) {}
+    constructor(private readonly mockFilms: MockFilms) {}
 
     public generate(): string {
-        const name = this.GetRandomItem<string>(this.mockData.names);
-        const description = this.GetRandomItem<string>(this.mockData.descriptions);
+        const name = this.GetRandomItem<string>(this.mockFilms.names);
+        const description = this.GetRandomItem<string>(this.mockFilms.descriptions);
         const postDate = new Date();
-        const genre = <Genre>this.GetRandomItem(this.mockData.genres);
-        const releaseYear = this.GetRandomItem<number>(this.mockData.releaseYears);
-        const rating = this.GetRandomItem<number>(this.mockData.ratings);;
-        const movie = this.GetRandomItem<string>(this.mockData.movies);
-        const actors = this.GetRandomItems<string>(this.mockData.actors).join(';');
-        const producer = this.GetRandomItem<string>(this.mockData.producers);
-        const movieDuration = this.GetRandomItem(this.mockData.movieDurations);
-        const userName = this.GetRandomItem<string>(this.mockData.userNames);
-        const userEmail = this.GetRandomItem<string>(this.mockData.userEmails);
-        const avatarPath = this.GetRandomItem<string>(this.mockData.avatarPaths);
-        const poster = this.GetRandomItem<string>(this.mockData.posterPaths);
-        const backgroundImage = this.GetRandomItem<string>(this.mockData.backgroundImages);
-        const backgroundColor = this.GetRandomItem<string>(this.mockData.backgroundColors);
+        const genre = <Genre>this.GetRandomItem(this.mockFilms.genres);
+        const releaseYear = this.GetRandomItem<number>(this.mockFilms.releaseYears);
+        const rating = this.GetRandomItem<number>(this.mockFilms.ratings);;
+        const movie = this.GetRandomItem<string>(this.mockFilms.movies);
+        const actors = this.GetRandomItems<string>(this.mockFilms.actors).join(';');
+        const producer = this.GetRandomItem<string>(this.mockFilms.producers);
+        const movieDuration = this.GetRandomItem(this.mockFilms.movieDurations);
+        const userName = this.GetRandomItem<string>(this.mockFilms.userNames);
+        const userEmail = this.GetRandomItem<string>(this.mockFilms.userEmails);
+        const avatarPath = this.GetRandomItem<string>(this.mockFilms.avatarPaths);
+        const poster = this.GetRandomItem<string>(this.mockFilms.posterPaths);
+        const backgroundImage = this.GetRandomItem<string>(this.mockFilms.backgroundImages);
+        const backgroundColor = this.GetRandomItem<string>(this.mockFilms.backgroundColors);
     
         return [
             name,
@@ -37,6 +37,7 @@ export default class MovieGenerator {
             userEmail,
             0,
             avatarPath,
+            "",
             poster,
             backgroundImage,
             backgroundColor,
--- src/common/tsv-file-reader.ts
+++ src/common/tsv-file-reader.ts
@@ -1,42 +1,52 @@
-import {readFileSync} from 'fs'
+import {createReadStream} from 'fs';
 import { Genre } from '../types/genre.enum.js';
 import { Movie } from '../types/movie.type.js';
 import { FileReaderInterface } from './file-reader.interface.js';
+import EventEmitter from 'events';
 
-export default class TSVFileReader implements FileReaderInterface {
-    private rawData = '';
-
-    constructor(public filename: string) { }
-
-    public read(): void {
-        this.rawData = readFileSync(this.filename, { encoding: 'utf8'});
+export default class TSVFileReader extends EventEmitter implements FileReaderInterface {
+    constructor(public filename: string) { 
+        super();
     }
 
-    public toArray(): Movie[] {
-        if (!this.rawData){
-            return [];
+    public async read(): Promise<void> {
+        const stream = createReadStream(this.filename, {encoding: 'utf-8'});
+        var lineRead = '';
+        var endLinePosition = -1;
+
+        for await (const chunk of stream) {
+            try {
+                lineRead += chunk.toString();
+            } catch { }
         }
 
-        return this.rawData
-            .split('\n')
-            .filter((row) => row.trim() !== '')
-            .map((line) => line.split('\\t'))
-            .map(([name, description, postDate, genre, releaseYear, rating, movie, actors, producer, movieDuration, commentsCount, userName, userEmail, userAvatarPath, userPassword, poster, backgroundImage, backgroundColor]) => ({
-                name,
-                description,
-                postDate: new Date(postDate),
-                genre: <Genre>genre,
-                releaseYear: Number(releaseYear),
-                rating: Number(rating),
-                movie,
-                actors: String(actors).split(';'),
-                producer,
-                movieDuration: Number(movieDuration),
-                commentsCount: Number(commentsCount),
-                user: {name: userName, email: userEmail, avatarPath: userAvatarPath, password: userPassword},
-                poster,
-                backgroundImage,
-                backgroundColor
-            }));
+        while ((endLinePosition = lineRead.indexOf('\n')) >= 0) {
+            const row = lineRead.slice(0, endLinePosition + 1);
+            lineRead = lineRead.slice(++endLinePosition);
+            this.emit('parsedMovie', this.ToMovie(row));
+        }
+    }
+
+    private ToMovie(str: string): Movie | undefined{
+        if (!str) return;
+        var params = str.split('\\t');
+
+        return {
+            name: params[0],
+            description: params[1],
+            postDate: new Date(params[2]),
+            genre: <Genre>params[3],
+            releaseYear: Number(params[4]),
+            rating: Number(params[5]),
+            movie: params[6],
+            actors: String(params[7]).split(';'),
+            producer: params[8],
+            movieDuration: Number(params[9]),
+            commentsCount: Number(params[10]),
+            user: {name: params[11], email: params[12], avatarPath: params[13], password: params[14]},
+            poster: params[15],
+            backgroundImage: params[16],
+            backgroundColor: params[17]
+            };
     }
 }
